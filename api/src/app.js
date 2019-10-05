19c5058aaa0677f0b079da1088e3fe9047153e7d
Ryan Albon <ryanalbon@gmail.com>
2019-10-05 11:31:01 +0200

    chore: Adding fake API endpoint that responds with random speeds and rates of success.

--- api/src/app.js
+++ api/src/app.js
@@ -4,4 +4,27 @@ const app = express();
 
 app.get('/status', (req, res) => res.status(200).end());
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getRandomResponseCode() {
+  const x = Math.floor(Math.random() * 100);
+
+  if (x < 90) {
+    return 200;
+  }
+
+  if (x < 98) {
+    return 400;
+  }
+
+  return 500;
+}
+
+app.get('/api/v1/gadgets', async (req, res) => {
+  await sleep(Math.floor(Math.random() * 5000));
+  res.status(getRandomResponseCode()).end();
+});
+
 module.exports = app;
